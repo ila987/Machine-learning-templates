00def8a9f9230c9ccb29479e6feafede34b73bbd
ila987 <ilaria_1987@hotmail.com>
2017-08-16 21:15:15 +0200

    KNN in R and Python

---
+++ 3- Classification/K-Nearest Neighbors (KNN)/python/K-nn.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  1 12:38:03 2017
+
+K-NN classifier
+
+@author: Ilaria
+"""
+
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, [2,3]].values
+y = dataset.iloc[:, 4].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+#fitting KNN classifier to the training set
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors=5, metric = 'minkowski', p=2)
+classifier.fit(X_train, y_train)
+
+
+#predict test set result
+y_pred = classifier.predict(X_test)
+
+#making the confusion matrix
+from sklearn.metrics import confusion_matrix 
+cm = confusion_matrix(y_test, y_pred)
+
+#visualizing the training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:,0].min() - 1, stop = X_set[:,0].max() + 1, step=0.01), 
+                     np.arange(start = X_set[:,1].min() - 1, stop = X_set[:,1].max() + 1, step=0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape), 
+             alpha = 0.75, cmap= ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X2.max())
+plt.ylim(X2.min(), X1.max())
+for i,j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set==j, 0], X_set[y_set==j, 1], 
+                c= ListedColormap(('red', 'green'))(i), label= j)
+
+plt.title('K-NN  (training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.show()
+
+
+#visualizing the test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:,0].min() - 1, stop = X_set[:,0].max() + 1, step=0.01), 
+                     np.arange(start = X_set[:,1].min() - 1, stop = X_set[:,1].max() + 1, step=0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape), 
+             alpha = 0.75, cmap= ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X2.max())
+plt.ylim(X2.min(), X1.max())
+for i,j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set==j, 0], X_set[y_set==j, 1], 
+                c= ListedColormap(('red', 'green'))(i), label= j)
+
+plt.title('K-NN (test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.show()
+
