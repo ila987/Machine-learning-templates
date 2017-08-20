e84fccbec81c96c199551c12014d3121ed273fa5
ila987 <ilaria_1987@hotmail.com>
2017-08-20 11:58:54 +0200

    Cross validation in python and R

---
+++ 10- Model Selection & boosting/K-fold Cross-Validation/python/grid_search.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 23 14:13:09 2017
+
+GRID SEARCH
+it can also be used to understand if our data are linear or not
+
+@author: Ilaria
+"""
+
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, [2,3]].values
+y = dataset.iloc[:, 4].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+#fitting logistic regression to the training set
+from sklearn.svm import SVC
+#rbf means gaussian
+classifier = SVC(kernel = 'rbf', random_state = 0)
+classifier.fit(X_train, y_train)
+
+
+#predict test set result
+y_pred = classifier.predict(X_test)
+
+#making the confusion matrix
+from sklearn.metrics import confusion_matrix 
+cm = confusion_matrix(y_test, y_pred)
+
+#applying K-fold cross validation
+from sklearn.cross_validation import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, 
+                             cv = 10)
+#get the avarage to have a better idea of the overall model performance
+accuracies.mean()
+#calculate variance
+accuracies.std()
+
+#Apply Grid Search to find the best model and the best parameters
+from sklearn.model_selection import GridSearchCV
+#create a dictionary with all possible values for the paramenters
+
+parameters = [{'C':[1, 10, 100, 1000], 'kernel':['linear']}, 
+               {'C':[1, 10, 100, 1000], 'kernel':['rbf'], 'gamma': [0.5, 0.1, 0.001]}]
+grid_search = GridSearchCV(estimator = classifier, 
+                           param_grid = parameters,
+                           scoring = 'accuracy', 
+                           cv = 10,
+                           n_jobs = -1)
+
+grid_search = grid_search.fit(X_train, y_train)
+best_accuracy = grid_search.best_score_
+best_parameters = grid_search.best_params_
+
+#visualizing the training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:,0].min() - 1, stop = X_set[:,0].max() + 1, step=0.01), 
+                     np.arange(start = X_set[:,1].min() - 1, stop = X_set[:,1].max() + 1, step=0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape), 
+             alpha = 0.75, cmap= ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X2.max())
+plt.ylim(X2.min(), X1.max())
+for i,j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set==j, 0], X_set[y_set==j, 1], 
+                c= ListedColormap(('red', 'green'))(i), label= j)
+
+plt.title('SVM - gauss kernel (training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.show()
+
+
+#visualizing the test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:,0].min() - 1, stop = X_set[:,0].max() + 1, step=0.01), 
+                     np.arange(start = X_set[:,1].min() - 1, stop = X_set[:,1].max() + 1, step=0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape), 
+             alpha = 0.75, cmap= ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X2.max())
+plt.ylim(X2.min(), X1.max())
+for i,j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set==j, 0], X_set[y_set==j, 1], 
+                c= ListedColormap(('red', 'green'))(i), label= j)
+
+plt.title('SVM - gauss kernel (test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.show()
+
+
+
